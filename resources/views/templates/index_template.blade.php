352bd584d5df9ed5cfde04e05d725b9e28627409
Vasyl <admin@gmail.com>
2021-01-08 20:31:59 +0200

    added some styles, included styles, created menu

--- resources/views/templates/index_template.blade.php
+++ resources/views/templates/index_template.blade.php
@@ -1,46 +1,29 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Diary</title>
-
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link rel="stylesheet" href="css/styles.css">
+        <title>Diary</title>
     </head>
     <body>
         <header>
-
+            <img src="/images/logo.png" alt="logo" id = "logo">
         </header>
         <nav>
-
+            <ul id = "menu">
+                <a href="">
+                    <li>My groups</li>
+                </a>
+                <a href="">
+                    <li>Marks</li>
+                </a>
+                <a href="">
+                    <li>Created group</li>
+                </a>
+                <a href="#">
+                    <li>Log out</li>
+                </a>
+            </ul>
         </nav>
         <main>
 
