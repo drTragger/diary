1b041f3a26d9b30fa4a19430e8ea703c493ea381
Someone <rgbjkbs@hdbf.ru>
2021-01-08 20:15:10 +0200

    Added migrations

---
+++ database/migrations/2021_01_08_170512_create_users_groups_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_groups', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('group_id');
+            $table->timestamps();
+            
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->foreign('group_id')
+                ->references('id')
+                ->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users_groups');
+    }
+}
---
+++ database/migrations/2021_01_08_170708_create_checked_answers_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCheckedAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checked_answers', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->unsignedInteger('teacher_id');
+            $table->unsignedInteger('answer_id');
+            $table->unsignedInteger('group_id');
+            $table->text('content');
+            $table->unsignedTinyInteger('score', false);
+            $table->timestamps();
+            
+            $table->foreign('teacher_id')
+                ->references('id')
+                ->on('users');
+            $table->foreign('answer_id')
+                ->references('id')
+                ->on('answers');
+            $table->foreign('group_id')
+                ->references('id')
+                ->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('checked_answers');
+    }
+}
