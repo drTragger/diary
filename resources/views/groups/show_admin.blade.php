7a309686e6d8fafb258547bf0cb1f26a802f7815
user <user@user.com>
2021-01-11 20:45:04 +0200

    views show

--- resources/views/groups/show_admin.blade.php
+++ resources/views/groups/show_admin.blade.php
@@ -1,26 +1,28 @@
 @extends('templates.admin')
 @section('content')
 
-@if(count($tasks)>0)
-@foreach ($tasks as $task)
-<div>
-    <div>
-        <div>
-            Homework# {{$task->id}}
-        </div>
-        <div>
-            Subject: {{$task->name}}
-        </div>
-        <div>
-            Date: {{$task->date_created}}
-        </div>
-    </div>
-    <div>
-        {{$task->content}}
-    </div>
-    <div>
-        Submitted: X students
-    </div>
-</div>
+    @if(count($tasks)>0)
+        @foreach ($tasks as $task)
+            <div>
+                <div>
+                    <div>
+                        Homework# {{$task->id}}
+                    </div>
+                    <div>
+                        Subject: {{$task->name}}
+                    </div>
+                    <div>
+                        Date: {{$task->date_created}}
+                    </div>
+                </div>
+                <div>
+                    {{$task->content}}
+                </div>
+                <div>
+                    Submitted: X students     {{--TODO get submitted tasks--}}
+                </div>
+            </div>
+        @endforeach
+    @endif
 @endsection
 
--- resources/views/groups/show_pupil.blade.php
+++ resources/views/groups/show_pupil.blade.php
@@ -1,17 +1,32 @@
+@extends('templates.index')
+@section('content')
 
-<!--todo if groups exist-->
-
-<table>
-    <tr>
-        <td>
+    @if(count($tasks)>0)
+        @foreach ($tasks as $task)
             <div>
-                Group Name
+                <div>
+                    <div>
+                        Homework# {{$task->id}}
+                    </div>
+                    <div>
+                        Subject: {{$task->name}}
+                    </div>
+                    <div>
+                        Date: {{$task->date_created}}
+                    </div>
+                </div>
+                <div>
+                    {{$task->content}}
+                </div>
+                <div>
+<form action="" method="POST" enctype="multipart/form-data">
+    {{ csrf_field() }}
+    <input type="file" name="task">
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+                </div>
             </div>
-        </td>
-        <td>
-            <button type="submit">
-                View
-            </button>
-        </td>
-    </tr>
-</table>
+        @endforeach
+    @endif
+@endsection
+
