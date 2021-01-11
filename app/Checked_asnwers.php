8362ff0baa3ed8b9ca7d6aaabd82af7006fe4d80
Someone <rgbjkbs@hdbf.ru>
2021-01-11 20:54:23 +0200

    added Checked_asnwers model

--- app/Checked_asnwers.php
+++ app/Checked_asnwers.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Checked_asnwers extends Model
 {
-    protected $fillable = [];
+    protected $fillable = ['id', 'teacher_id', 'answer_id', 'group_id', 'content', 'score', 'date_created', 'date_update'];
     
     public function user() {
         return $this->belongsTo('App\User');
