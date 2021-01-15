e91c1dfa32c26e1861c719fedf8d6e50486b4abe
ReznichenkoKirill <kirill.reznichenkoo@gmail.com>
2021-01-15 19:00:26 +0200

    changed views

---
+++ resources/views/templates/topnav.blade.php
@@ -0,0 +1,14 @@
+<li>
+    <a href="#" class="btn main-nav-a-btn">Ученики</a>
+</li>
+<li>
+    <a href="{{route('groups.selectUser', $group->id)}}" class="btn main-nav-a-btn">Добавить ученика</a>
+</li>
+<li>
+    <form action="{{route('groups.delete', $group->id)}}" method="POST">
+        {{csrf_field()}}
+        {{method_field('DELETE')}}
+        <input type="submit" value="Удалить группу" class="btn main-nav-a-btn">
+    </form>
+    {{--        <a href="{{route('groups.delete', $group->id)}}" class="btn main-nav-a-btn">Удалить группу</a>--}}
+</li>
