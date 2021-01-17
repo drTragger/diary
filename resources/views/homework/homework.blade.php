7eb3fdeac789a9bc0e831c69c736bab522475ce7
kate <ketrina113@gmail.com>
2021-01-17 21:42:01 +0200

    view homework

---
+++ resources/views/homework/homework.blade.php
@@ -0,0 +1,29 @@
+@extends('templates.default')
+
+@section('content')
+    @if(count($tasks)>0)
+        @foreach ($tasks as $task)
+            <div>
+                <div>
+                    <div>
+                        Homework# {{$task->id}}
+                    </div>
+                    <div>
+                        Subject: {{$task->name}}
+                    </div>
+                    <div>
+                        Date: {{$task->date_created}}
+                    </div>
+                </div>
+                <div>
+                    {{$task->content}}
+                </div>
+                <div>
+                    Submitted: X students     {{--TODO get submitted tasks--}}
+                </div>
+            </div>
+        @endforeach
+    @endif
+@endsection
+
+@endsection
