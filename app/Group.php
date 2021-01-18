6e2f4bc0e632e4fe949175760c1ead52058bcdd8
misha <mishaponomarenko11082001@gmail.com>
2021-01-18 20:34:44 +0200

    Added a feature to create homework tasks

--- app/Group.php
+++ app/Group.php
@@ -1,21 +1,28 @@
 <?php
-    namespace App;
-    use Illuminate\Database\Eloquent\Model;
-    class Group extends Model
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    const ACTIVE = 1;
+    const INACTIVE = 2;
+
+    protected $fillable = ['name', 'description', 'owner_id', 'status',];
+
+    public function user()
     {
-        const ACTIVE = 1;
-        const INACTIVE = 2;
-        
-        protected $fillable = ['name', 'description', 'owner_id', 'status',];
-        public function user()
-        {
-            return $this->belongsTo('App\User', 'owner_id');
-        }
-        public function students(){
-            return $this->belongsToMany('App\User', 'users_groups', 'group_id', 'user_id');
-        }
-//
-//        public function tasks() {
-//            return $this->hasMany('App\Task', 'group_id');
-//        }
+        return $this->belongsTo('App\User', 'owner_id');
     }
+
+    public function students()
+    {
+        return $this->belongsToMany('App\User', 'users_groups', 'group_id', 'user_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Task', 'group_id');
+    }
+}
---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'subject' => 'required|max:255',
+            'task' => 'required|max:65535',
+        ];
+    }
+}
