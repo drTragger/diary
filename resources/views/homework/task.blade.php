df1592b7645596d03354bee7497ddc399fbaae06
misha <mishaponomarenko11082001@gmail.com>
2021-01-18 20:50:24 +0200

    Added styles for the form for adding tasks

--- resources/views/homework/task.blade.php
+++ resources/views/homework/task.blade.php
@@ -1,17 +1,19 @@
 @extends('templates.default')
 
 @section('content')
-    <form action="{{ route('homework.addTask') }}" method="POST">
-        {{ csrf_field() }}
-        <input type="hidden" name="groupId" value="{{ $groupId }}">
-        <div class="form-group">
-            <label for="subject">Subject</label>
-            <input type="text" name="subject" id="subject" class="form-control" required maxlength="60">
-        </div>
-        <div class="form-group">
-            <label for="task">Task</label>
-            <textarea name="task" id="task" class="form-control" cols="30" rows="10"></textarea>
-        </div>
-        <button type="submit" class="btn btn-default">Add</button>
-    </form>
+    <div class="add-task">
+        <form action="{{ route('homework.addTask') }}" method="POST">
+            {{ csrf_field() }}
+            <input type="hidden" name="groupId" value="{{ $groupId }}">
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <input type="text" name="subject" id="subject" class="form-control" required maxlength="60">
+            </div>
+            <div class="form-group">
+                <label for="task">Task</label>
+                <textarea name="task" id="task" class="form-control" cols="30" rows="10"></textarea>
+            </div>
+            <button type="submit" class="btn btn-default">Add</button>
+        </form>
+    </div>
 @endsection
