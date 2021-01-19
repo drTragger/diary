239a50bfc9673e0a87423b90ba83f2e7eab29478
ReznichenkoKirill <kirill.reznichenkoo@gmail.com>
2021-01-19 02:39:10 +0200

    changed view topnav

--- resources/views/templates/topnav.blade.php
+++ resources/views/templates/topnav.blade.php
@@ -17,11 +17,5 @@
     <a href="#" class="btn main-nav-a-btn">Marks</a>
 </li>
 <li>
-    <a href="#" class="btn main-nav-a-btn">Delete group</a>
-    {{--    <form action="{{route('groups.delete', $group->id)}}" method="POST">--}}
-    {{--        {{csrf_field()}}--}}
-    {{--        {{method_field('DELETE')}}--}}
-    {{--        <input type="submit" value="Удалить группу" class="btn main-nav-a-btn">--}}
-    {{--    </form>--}}
-    {{--        <a href="{{route('groups.delete', $group->id)}}" class="btn main-nav-a-btn">Удалить группу</a>--}}
+    <a href="{{route('groups.confirmDeactivate', $group)}}" class="btn main-nav-a-btn">deactivate group</a>
 </li>
