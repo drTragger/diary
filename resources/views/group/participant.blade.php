59f678cee8e8b5147100d7d45197826910e13c41
ReznichenkoKirill <kirill.reznichenkoo@gmail.com>
2021-01-19 02:13:37 +0200

    changed view participant

--- resources/views/group/participant.blade.php
+++ resources/views/group/participant.blade.php
@@ -4,26 +4,25 @@
 @endsection
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div>
-                    @include('common.errors')
-                </div>
-                <div>
-                    <form action="{{route('groups.addUser')}}" method="POST" class="d-flex flex-direction-column">
-
-                        {{csrf_field()}}
-                        {{method_field('PUT')}}
-                        <label for="participant">Электронная почта ученика</label>
-                        <input type="text" name="email" id="participant">
-                        <input type="hidden" name="id" value="{{$group->id}}">
-                        <input type="submit" name="Add participant">
-                    </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div>
+                        @include('common.errors')
+                    </div>
+                    <div>
+                        <form action="{{route('groups.addUser')}}" method="POST" class="d-flex flex-direction-column">
+                            {{csrf_field()}}
+                            {{method_field('patch')}}
+                            <label for="participant">Электронная почта ученика</label>
+                            <input type="text" name="email" id="participant">
+                            <input type="hidden" name="id" value="{{$group->id}}">
+                            <input type="submit" name="Add participant">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
