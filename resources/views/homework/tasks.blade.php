c818bfa14ce591b3e208ecd8b0d2f038e7bc9144
misha <mishaponomarenko11082001@gmail.com>
2021-01-22 19:56:38 +0200

    Changed homework show

--- resources/views/homework/tasks.blade.php
+++ resources/views/homework/tasks.blade.php
@@ -4,9 +4,22 @@
         @if(!empty ($tasks))
             @foreach($tasks as $task)
                 <div class="col-6 group-item">
+                    @if($check)
+                        <form action="{{ route('homework.taskEdition', ['task' => $task->id]) }}"
+                              method="post">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="group_id" value="{{ $group->id }}">
+                            <button type="submit" class="btn btn-default">Edit</button>
+                        </form>
+                    @endif
                     <h4>{{$task->name}}</h4>
                     <p>{{$task->content}}</p>
                     <p>{{$task->created_at}}</p>
+                    @if($check)
+                        @include('homework.homeworkOwner')
+                    @else
+                        @include('homework.homeworkStudent')
+                    @endif
                 </div>
             @endforeach
 
