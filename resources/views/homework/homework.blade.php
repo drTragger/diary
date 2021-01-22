e9cd307a1b55d79b1872aa6b5f94a3362fc180a6
misha <mishaponomarenko11082001@gmail.com>
2021-01-22 19:05:48 +0200

    Fixed bugs

--- resources/views/homework/homework.blade.php
+++ resources/views/homework/homework.blade.php
@@ -12,7 +12,12 @@
                         <th>#</th>
                         <th>Title</th>
                         <th>Date</th>
-                        <th>link</th>
+                        @if(\Illuminate\Support\Facades\Auth::user()->id === $group->owner_id)
+                            <th>Submitted</th>
+                            <th>Action</th>
+                        @else
+                            <th>Link</th>
+                        @endif
                     </tr>
                     </thead>
                     <tbody>
@@ -27,47 +32,23 @@
                                 @else
                                     @include('homework.homeworkStudent')
                                 @endif
-{{--                                <form action="{{route('homework.show',[$group->id, $task])}}" method="post">--}}
-{{--                                    {{csrf_field()}}--}}
-{{--                                    --}}{{--                                        {{method_field('get')}}--}}
-{{--                                    <input type="hidden" name="task" value="{{$task}}">--}}
-{{--                                    <input type="hidden" name="group_id" value="{{$group->id}}">--}}
-{{--                                    <input type="hidden" name="task_content" value="{{$task->content}}">--}}
-{{--                                    <input type="hidden" name="task_name" value="{{$task->name}}">--}}
-{{--                                    <input type="submit" value="TODO">--}}
-{{--                                </form>--}}
                             </td>
+                            @if(\Illuminate\Support\Facades\Auth::user()->id === $group->owner_id)
+                                <td>
+                                    <form action="{{ route('homework.taskEdition', ['task' => $task->id]) }}"
+                                          method="post">
+                                        {{ csrf_field() }}
+                                        <input type="hidden" name="group_id" value="{{ $group->id }}">
+                                        <button type="submit" class="btn btn-default">Edit</button>
+                                    </form>
+                                </td>
+                            @endif
                         </tr>
                     @endforeach
                     </tbody>
                 </table>
-                @foreach ($tasks as $task)
-                    <div class="d-flex flex-column bd-highlight ">
-                        <div class="group-item w-75">
-                            @if(\Illuminate\Support\Facades\Auth::user()->id === $group->owner_id)
-                                <form action="{{ route('homework.taskEdition', ['task' => $task->id]) }}" method="post">
-                                    {{ csrf_field() }}
-                                    <input type="hidden" name="group_id" value="{{ $group->id }}">
-                                    <button type="submit" class="btn btn-default">Edit</button>
-                                </form>
-                            @endif
-                            <div>
-                                <p>Homework# {{$task->id}}</p>
-                                <p>Subject: {{$task->name}}</p>
-                                <p>Date: {{mb_strimwidth($task->created_at,0,10)}}</p>
-                                <p>{{$task->content}}</p>
-                            </div>
-                            <div class="container-a-bnt-info d-flex flex-direction-column">
-                                @yield('result')
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-        <div class="pages">{{$tasks->render()}}</div>
-    @else
-        <p class="row margin-0-auto ">No homework</p>
+                @else
+                    <p class="row margin-0-auto ">No homework</p>
     @endif
 @endsection
 
