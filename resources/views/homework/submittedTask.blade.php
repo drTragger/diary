bd511ff5c24a1f836102946073dd0f34ccf699b6
kate <ketrina113@gmail.com>
2021-01-24 23:58:51 +0200

    view submittedTask edit

--- resources/views/homework/submittedTask.blade.php
+++ resources/views/homework/submittedTask.blade.php
@@ -1,30 +1,31 @@
 @extends('templates.default')
 @section('nav')
-        @include('templates.topnav')
+    @include('templates.topnav')
 @endsection
 @section('content')
-
     @if(!empty($tasks))
-        <table class="table">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Subject</th>
-                <th>Estimate</th>
-            </tr>
-            </thead>
-            <tbody class="table">
-            @foreach($tasks as $task)
+        <div class="container-form">
+            <table class="table">
+                <thead>
                 <tr>
-                    <td>{{$task->id}}</td>
-                    <td>{{$task->name}}</td>
-                    <td>
-                        <a href="{{ route('homework.estimate', $task->id) }}" class="btn main-nav-a-btn">Estimate</a>
-                    </td>
+                    <th>#</th>
+                    <th>Subject</th>
+                    <th>Estimate</th>
                 </tr>
-            @endforeach
-            </tbody>
-
-        </table>
+                </thead>
+                <tbody class="table">
+                @foreach($tasks as $task)
+                    <tr>
+                        <td>{{$task->id}}</td>
+                        <td>{{$task->name}}</td>
+                        <td>
+                            <a href="{{ route('homework.estimate', $task->id) }}"
+                               class="btn main-nav-a-btn">Estimate</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     @endif
 @endsection
