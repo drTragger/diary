db719b18e2c6ab68ac999d270a9becbdbef937c5
kate <ketrina113@gmail.com>
2021-01-24 23:59:32 +0200

    view estimate edit

--- resources/views/homework/estimate.blade.php
+++ resources/views/homework/estimate.blade.php
@@ -3,32 +3,33 @@
     @include('templates.topnav')
 @endsection
 @section('content')
-
     @if(!empty($answers))
-        <table class="table">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Participant</th>
-                <th>Answer</th>
-                <th>Mark</th>
-            </tr>
-            </thead>
-            <tbody class="table">
-            @foreach($answers as $answer)
+        <div class="container-form">
+            <table class="table">
+                <thead>
                 <tr>
-                    <td>{{$answer->id}}</td>
-                    <td>{{$answer->owner_id}}</td>
-                    <td>{{$answer->content}}</td>
-                    <td>
-                        <form action="" method="post">
-                            <input type="number" name="mark" required>
-                            <input type="submit" value="Save" class="btn a-btn-info align-self-center">>
-                        </form>
-                    </td>
+                    <th>#</th>
+                    <th>Participant</th>
+                    <th>Answer</th>
+                    <th>Mark</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody class="table">
+                @foreach($answers as $answer)
+                    <tr>
+                        <td>{{$answer->id}}</td>
+                        <td>{{$answer->name}}</td>
+                        <td>{{$answer->content}}</td>
+                        <td>
+                            <form action="" method="post">
+                                <input type="number" name="mark" placeholder="Enter the mark" required>
+                                <input type="submit" value="Save" class="btn a-btn-info align-self-center">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     @endif
 @endsection
