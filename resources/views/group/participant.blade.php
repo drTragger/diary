bdf133f936a48c244066b66ba69900691fe088a8
kate <ketrina113@gmail.com>
2021-01-27 00:29:55 +0200

    edit style in group\participant

--- resources/views/group/participant.blade.php
+++ resources/views/group/participant.blade.php
@@ -1,16 +1,16 @@
 @extends('templates.default')
 @section('back')
-    <a href="{{route('groups.showParticipants', $group->id)}}" class="btn btn-secondary">Back</a>
+    <a href="{{route('groups.showParticipants', $group->id)}}" class="btn btn-warning">Back</a>
 @endsection
 @section('content')
-    <div class="panel panel-default col-12">
+    <div class="panel panel-default col-12 ">
         <div>
             @include('common.errors')
         </div>
         @if(!empty(session('mess')))
             <div class="bg-warning pt-2 pb-2 text-center mb-3">{{session('mess')}}</div>
         @endif
-        <div>
+        <div class="fill-bg">
             <form action="{{route('groups.addUser')}}" method="POST" class="text-center">
                 {{csrf_field()}}
                 {{method_field('patch')}}
