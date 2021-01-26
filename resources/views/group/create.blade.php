d2faf182606de62d4cbea092c22f0a4ffbd8a1e8
kate <ketrina113@gmail.com>
2021-01-27 00:22:34 +0200

    edit style in group\create

--- resources/views/group/create.blade.php
+++ resources/views/group/create.blade.php
@@ -1,22 +1,29 @@
 @extends('templates.default')
-
+@section('back')
+    <a href="{{route('groups.index')}}" class="btn btn-warning">Back</a>
+@endsection
 @section('content')
     @include('common.errors')
-        <div class="container-form d-flex flex-wrap justify-content-center">
-            <h3 class="text-center col-12">Create new group</h3>
-            <form action="{{route('groups.add')}}" method="POST" class="mt-5 text-center col-12">
+    <div class="container-form d-flex flex-wrap justify-content-center">
+        <div class="container-form d-inline-flex flex-wrap  fill-bg">
+                <h3 class="text-center col-12">Create new group</h3>
+            <form action="{{route('groups.add')}}" method="POST" class="mt-5 col-12">
                 {{csrf_field()}}
                 {{method_field('PUT')}}
                 <div class="form-group">
-                    <label for="name">Name</label>
+                    <label for="name">Name:</label>
                     <input type="text" name="name" id="name" class="form-control">
                 </div>
-                <div class="form-group">
-                    <label for="description">Description</label>
+                <div class="form-group ">
+                    <label for="description">Description:</label>
                     <textarea name="description" id="description" class="form-control"></textarea>
                 </div>
                 <input type="hidden" name="status" value="on">
-                <button type="submit" class="btn btn-success">Create</button>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-success text-center">Create</button>
+                </div>
             </form>
         </div>
+    </div>
+
 @endsection
