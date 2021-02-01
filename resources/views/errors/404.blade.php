2dfe67be439925e41c9632b2b50d55ff78808a22
misha <mishaponomarenko11082001@gmail.com>
2021-02-01 18:04:47 +0200

    Changed Not found style

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -39,6 +39,7 @@
             -webkit-transform: translate(-50%, -50%);
             -ms-transform: translate(-50%, -50%);
             transform: translate(-50%, -50%);
+            user-select: none;
         }
 
         .notfound {
