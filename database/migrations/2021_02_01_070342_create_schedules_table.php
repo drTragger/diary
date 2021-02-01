6642232ad09e433b81de2fe866fa5df8c718d7d3
ReznichenkoKirill <kirill.reznichenkoo@gmail.com>
2021-02-01 18:23:16 +0200

    added migrations

---
+++ database/migrations/2021_02_01_070342_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->unsignedInteger('group_id');
+            $table->timestamp('start_at');
+            $table->timestamp('end');
+            $table->timestamps();
+
+            $table->foreign('group_id')
+                ->references('id')
+                ->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('schedules');
+    }
+}
