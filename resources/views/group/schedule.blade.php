570904943217163728486ec0994f89127f57d3e2
user <user@user.com>
2021-02-01 20:52:28 +0200

    edit view schedule

--- resources/views/group/schedule.blade.php
+++ resources/views/group/schedule.blade.php
@@ -3,22 +3,5 @@
     <a href="{{route('groups.index')}}" class="btn btn-warning"><i class="fas fa-long-arrow-alt-left"></i></a>
 @endsection
 @section('content')
-    @include('common.errors')
-
-    <table class="table">
-        <thead>
-        <tr>
-            <td>Mo</td>
-            <td>Tu</td>
-            <td>We</td>
-            <td>Th</td>
-            <td>Fr</td>
-            <td>Sa</td>
-            <td>Su</td>
-        </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-    </table>
+    <div id='calendar' class="fill-bg"></div>
 @endsection
